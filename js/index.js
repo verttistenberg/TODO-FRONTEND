24aeb404837c842475207626b424ae84076b5c40
verttistenberg <vertti.stenberg@gmail.com>
2026-03-23 00:51:24 +0200

    Update backend url

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
+const BACKEND_ROOT_URL = 'https://todo-backend-1-uzc5.onrender.com/'
 import { Todos } from "./class/Todos.js"
 
-const BACKEND_ROOT_URL = 'http://localhost:3001'
 const todos = new Todos(BACKEND_ROOT_URL)
 const list = document.querySelector('ul');
 const input = document.querySelector('input');
